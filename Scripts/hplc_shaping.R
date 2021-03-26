08a0e8cfae331e5e4b8d6b3779931e1316311c84
Flavi1P <flavien.petit@obs-vlfr.fr>
2021-03-26 16:00:59 +0100

    the script now compile all hplc database, still some issues with dates

--- Scripts/hplc_shaping.R
+++ Scripts/hplc_shaping.R
@@ -1,5 +1,5 @@
 library(readxl)
-library(tidyverse)
+library(tidyverse, lubridate)
 
 #extract path to HPLC excel sheets, only one sheet per campagne ####
 folders <- list.dirs('.', recursive=TRUE, full.names = TRUE) #list all folders in the raw excel one
@@ -21,52 +21,333 @@ for(i in HPLC_files){
 } #remove duplicate files from my paths
 
 my_path <- my_path[grep('\\.xls', my_path)]
+my_path <- my_path[-grep('autre', my_path)]
 my_path <- c(my_path, HPLC_files_single)
 rm(files, HPLC_files_single, folder, HPLC_folders, HPLC_files, unique_hplc, check, i, test, folders)
 
-#store all possibles colnames ####
+#create a df with all pigments ####
 cruises_data <- tibble('path' = my_path,
-                       'cruise' = c('soclim', 'lovbio001i', 'boussole', 'labrador', 'icb', 'dewex', 'irs', 'dewex', 'oiso', 'lovbio059c', 'lovbio061c', 'moose', 'lovbio064b', 'outpace', 'bioargomed', 'liste_flotteurs', 'greenedge', 'mobydick', 'soclim'))
+                       'cruise' = c('soclim', 'boussole', 'labrador', 'icb', 'dewex', 'irs', 'dewex', 'oiso', 'lovbio059c', 'lovbio061c', 'moose', 'outpace', 'bioargomed', 'liste_flotteurs', 'greenedge', 'mobydick', 'soclim'))
 
-list_colname <- c()
-test <- tibble('cruise_name' = character(),
+data_large <- tibble('cruise_name' = character(),
                    'date' = character(),
                    'lat' = numeric(),
                    'lon' = numeric(),
                    'station_name' = character(),
-                   'ctd_number' = numeric(),
+                   'ctd_number' = character(),
                    'bottle_number' = numeric(),
                    'depth' = numeric(),
-                   'pigment' = character(),
-                   'concentration' = character(),
-                   'flag' = character()
-                   )
-for (i in length(cruises_data$path)){
+                   'chl_c1_c2' = character(),
+                    'chl_c3' = character(),
+               'peri' = character(),
+               'but' = character(),
+               'fuco' = character(),
+               'neox' = character(),
+               'prasi' = character(),
+               'viola' = character(),
+               'hex' = character(),
+               'diad' = character(),
+               'allo' = character(),
+               'diat' = character(),
+               'zea' = character(),
+               'lutein' = character(),
+               'dv_chlb' = character(),
+               'chlb' = character(),
+               'tchlb' = character(),
+               'dv_chla' = character(),
+               'chla' = character(),
+               'tchla' = character(),
+               'phaeo' = character())
+for (i in 1:length(cruises_data$path)){
   path <- as.character(cruises_data[i,1])
   
   data <- read_excel(path) %>% janitor::clean_names()
-  # column <- colnames(data)
-  # list_colname <- append(list_colname, column)
-  # list_colname <- unique(list_colname)
+
   cruise_name <- cruises_data[i,2]
-  date <- select(data, matches('(^.+date)|date'))
-  ship <- select(data, matches('r/v|ship'))
+  
+  date <- select(data, matches('(^.+date)|(^date$)'))
+  if(length(date) != 1){
+    if(length(date) == 2){
+      date <- select(date, matches('utc'))
+    }
+    if(length(date) == 0){
+      year <- select(data, matches('year'))
+      month <- select(data, matches('month'))
+      day <- select(data, matches('day'))
+      date <- paste(day[[1]], month[[1]], year[[1]], sep = '/')
+    }
+  }
+  
+  lat <- select(data, matches('lat'))
+  if(length(lat)!=1){
+    lat <- lat[,1]
+    if(length(lat) != 1){
+      print(i)
+      break()
+    }
+  }
+  
+  lon <- select(data, matches('lon'))
+  if(length(lon) != 1){
+    lon <- lon[,1]
+    if(length(lon) != 1){
+      print(i)
+      break()
+    }
+  }
+  
+  ctd <- select(data, matches('ctd'))
+  if(length(ctd) != 1){
+    print(i)
+    break()
+  }
+  
+  station <- select(data, matches('station'))
+  if(length(station) != 1){
+    station <- date
+    print(paste('station = date for ', path))
+  }
+  
+  btl <- select(data, matches('bottle'))
+  if(length(btl) != 1){
+    print(paste('bottle is NA for ', path))
+    btl <- NA
+  }  
+  
+  depth <- select(data, matches('depth'))
+  if(length(depth) != 1){
+    depth <- select(data, matches('^depth$'))
+    if(length(depth) != 1){
+      print(path)
+      break()
+    }
+
+  }
+  chl_c1_c2 <- NA
+  chl_c3 <- NA
+  peri <- NA
+  but <- NA
+  fuco <- NA
+  neox <- NA
+  prasi <- NA
+  viola <- NA
+  hex <- NA
+  diad <- NA
+  allo <- NA
+  diat <- NA
+  zea <- NA
+  lutein <- NA
+  dv_chlb <- NA
+  chlb <- NA
+  tchlb <- NA
+  dv_chla <- NA
+  chla <- NA
+  tchla <- NA
+  if(i != 14){
+    
+    chl_c1_c2 <- select(data, matches('(chl.*c1.*c2)$|(chl.*c2.*c1)$|(chl.*c2.*c1).*[^a]$|(chl.*c1.*c2).*[^a]$'))
+    if(length(chl_c1_c2) != 1){
+      chl_c1 <- select(data, matches('chl.+c1$'))
+      chl_c2 <- select(data, matches('chl.+c2$'))
+      chl_c1_c2 <- chl_c1 + chl_c2
+      if(length(chl_c1) !=1 | length(chl_c2) !=1){
+        print(path)
+        break() 
+      }
+    }
+    
+    chl_c3 <- select(data, matches('^chl.*c3$'))
+    if(length(chl_c3) !=1){
+      print(paste('wrong regex for chl c3 on ', path))
+      break()
+    }
+    
+    peri <- select(data, matches('peridinin'))
+    if(length(peri) !=1){
+      print(paste('wrong regex for peri on ', path))
+      break()
+    }
+    
+    but <- select(data, matches('(?=.*but)(?!.*qa)', perl = TRUE))
+    if(length(but) !=1){
+      print(paste('wrong regex for but on ', path))
+      break()
+    }
+    
+    fuco <- select(data, matches('^(?=fuco)(?!.*qa)', perl = TRUE))
+    if(length(but) !=1){
+      print(paste('wrong regex for fuco on ', path))
+      break()
+    } 
+    
+    neox <- select(data, matches('^(?=neox)(?!.*qa)', perl = TRUE))
+    if(length(neox) !=1){
+      print(paste('wrong regex for neox on ', path))
+      break()
+    }
+    
+    prasi <- select(data, matches('^(?=prasi)(?!.*qa)', perl = TRUE))
+    if(length(prasi) !=1){
+      print(paste('wrong regex for prasi on ', path))
+      break()
+    }
+    
+    viola <- select(data, matches('^(?=viola)(?!.*qa)', perl = TRUE))
+    if(length(viola) !=1){
+      print(paste('wrong regex for viola on ', path))
+      break()
+    }
+    
+    hex <- select(data, matches('(?=hex)(?!.*qa)', perl = TRUE))
+    if(length(hex) !=1){
+      print(paste('wrong regex for hex on ', path))
+      break()
+    }
+    
+    diad <- select(data, matches('(?=diad)(?!.*qa)', perl = TRUE))
+    if(length(diad) !=1){
+      print(paste('wrong regex for diad on ', path))
+      break()
+    }
+    
+    allo <- select(data, matches('(?=allo)(?!.*qa)', perl = TRUE))
+    if(length(allo) !=1){
+      print(paste('wrong regex for allo on ', path))
+      break()
+    }
+    
+    diat <- select(data, matches('(?=diat)(?!.*qa)', perl = TRUE))
+    if(length(diat) !=1){
+      print(paste('wrong regex for diat on ', path))
+      break()
+    }
+    
+    zea <- select(data, matches('(?=zea)(?!.*qa)', perl = TRUE))
+    if(length(allo) !=1){
+      print(paste('wrong regex for zea on ', path))
+      break()
+    }
+    
+    lutein <- select(data, matches('^(?=lutein)(?!.*qa)', perl = TRUE))
+    if(length(lutein) !=1){
+      print(paste('wrong regex for lutein on ', path))
+      break()
+    }
+    
+    dv_chlb <-  select(data, matches('(?=div.*b$)(?!.*qa)', perl = TRUE))
+    if(length(dv_chlb) !=1){
+      if(cruise_name %in% c('dewex', 'lovbio059c', 'lovbio061c', 'lovbio063c', 'moose', 'greenedge')){
+        dv_chlb <- NA
+      }
+      else{
+        print(paste('wrong regex for dv_chlb on ', path))
+        break()
+      }
+    }
+    
+    chlb <- select(data, matches('^(?=chl.*b$)(?!.*qa)', perl = TRUE))
+    if(length(chlb) !=1){
+      if(cruise_name %in% c('dewex', 'lovbio059c', 'lovbio061c', 'lovbio063c', 'moose')){
+        chlb <- NA
+      }
+      else{
+      print(paste('wrong regex for chlb on ', path))
+      break()
+      }
+    }
+    
+    tchlb <- select(data, matches('^(?=t.*chl.*b$)(?!.*qa)', perl = TRUE))
+    if(cruise_name == 'greenedge'){
+      tchlb <- chlb
+    }
+    if(length(tchlb) !=1){
+      print(paste('wrong regex for tchlb on ', path))
+      break()
+    }
+    
+    dv_chla <- select(data, matches('(?=div.*a$)(?!.*qa)', perl = TRUE))
+    if(length(dv_chla) !=1){
+      print(paste('wrong regex for dv_chla on ', path))
+      break()
+    }
+    
+    chla <- select(data, matches('^(?!chlorophyllid)(?=chl.*a$)(?!.*qa)', perl = TRUE))
+    if(length(chla) !=1){
+      print(paste('wrong regex for chla on ', path))
+      break()
+    }
+    
+    tchla <- select(data, matches('^(?=t.*chl.*a$)(?!.*qa)', perl = TRUE))
+    if(length(tchla) !=1){
+      print(paste('wrong regex for tchla on ', path))
+      break()
+    }
+    
+    phaeophytin <- select(data, matches('(?=phaeophytin)(?!.*qa)', perl = TRUE))
+    if(length(phaeophytin) !=1){
+      print(paste('wrong regex for phaeophytin on ', path))
+      break()
+    }
+    
+  }
   
   
-  data_to_bind <- tibble('cruise_name' = cruise_name)
+  data_interm <- tibble('cruise_name' = cruise_name[[1]],
+                        'date' = as.character(date[[1]]),
+                        'lon' = as.numeric(lon[[1]]),
+                        'lat' = as.numeric(lat[[1]]),
+                        'ctd_number' = as.character(ctd[[1]]),
+                        'station_name' = as.character(station[[1]]),
+                        'bottle_number' = as.numeric(btl[[1]]),
+                        'chl_c1_c2' = as.character(chl_c1_c2[[1]]),
+                        'chl_c3' = as.character(chl_c3[[1]]),
+                        'peri' = as.character(peri[[1]]),
+                        'but' = as.character(but[[1]]),
+                        'fuco' = as.character(fuco[[1]]),
+                        'neox' = as.character(neox[[1]]),
+                        'prasi' = as.character(prasi[[1]]),
+                        'viola' = as.character(viola[[1]]),
+                        'hex' = as.character(hex[[1]]),
+                        'diad' = as.character(diad[[1]]),
+                        'allo' = as.character(allo[[1]]),
+                        'diat' = as.character(diat[[1]]),
+                        'zea' = as.character(zea[[1]]),
+                        'lutein' = as.character(lutein[[1]]),
+                        'dv_chlb' = as.character(dv_chlb[[1]]),
+                        'chlb' = as.character(chlb[[1]]),
+                        't_chlb' = as.character(tchlb[[1]]),
+                        'dv_chla' = as.character(dv_chla[[1]]),
+                        'chla' = as.character(chla[[1]]),
+                        't_chla' = as.character(tchla[[1]]),
+                        'depth' = abs(as.numeric(depth[[1]])))
+  data_large <- bind_rows(data_large, data_interm)
   
-  test <- bind_rows(test, data)
 }
 
-check_colname <- function(regexp, data){
-  result <- data[grep(regexp, data)]
-  return(result)
-}
+data_long <- data_large %>% pivot_longer(chl_c1_c2:t_chla, names_to = 'pigment', values_to = 'concentration')
+
+data_long$concentration <- tolower(data_long$concentration)
+data_long$concentration[data_long$concentration == 'lod'] <- 0
 
 
 
-check_colname('^t.*[^q]a$', list_colname)
-check_colname('^(chl)[^q]*a$', list_colname)
-check_colname('^(chl)[^q]*b$', list_colname)
+table(is.na(data_long$concentration))
+data_long$concentration <- as.numeric(data_long$concentration)
 
-test <- read_excel(i) %>% janitor::clean_names()
+data_long$concentration[data_long$concentration < 0] <- NA
+
+
+date_diff <- grep('^([0-9]{4}\\-(0?[1-9]|1[0-2])\\-(0?[1-9]|[1-2][0-9]|3[0-1]))$', data_long$date)
+
+date_num <- data_long$date[grep('^[0-9]{5}$', data_long$date)]
+date_ok <-  data_long$date[grep('^([0-9]{4}\\-(0?[1-9]|1[0-2])\\-(0?[1-9]|[1-2][0-9]|3[0-1]))$', data_long$date)]
+
+date_ok <- as_date(date_ok)
+
+date_num_date <- as.Date(as.numeric(date_num), origin = '1899-10-31')
+
+
+other_format <- unique(data_long$date[-date_diff])
+
+max(date_num)
+
